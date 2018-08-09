ec78f22890a2e13c5036b6afb05e65cf61159c49
George Allen <george.allen93@outlook.com>
2018-08-09 20:43:33 +0100

    Basic string bounds

--- lib/jfuzz/generators/string_generator.rb
+++ lib/jfuzz/generators/string_generator.rb
@@ -5,11 +5,21 @@ require "jfuzz/generators/generator"
 module Jfuzz 
   class StringGenerator < Generator
     def generate
-      ""
+      (lower_bound...upper_bound).map { (65 + rand(26)).chr }.join
     end
 
     def self.type
       "string"
+    end
+
+    private
+
+    def upper_bound
+      rand(0..100)
+    end
+
+    def lower_bound
+      0
     end
   end
 end
