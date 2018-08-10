3f3b81a0a0990837800a906314f42a8b8982a3a0
George Allen <george.allen93@outlook.com>
2018-08-10 20:15:55 +0100

    Better handling of strings

--- lib/jfuzz/generators/string_generator.rb
+++ lib/jfuzz/generators/string_generator.rb
@@ -1,31 +1,26 @@
 # frozen_string_literal: true
 
 require "regexp-examples"
+require "date"
 require "jfuzz/generators/generator"
 
 module Jfuzz 
   class StringGenerator < Generator
 
-    DATE_REGEXP = /\A\d{4}-\d{2}-\d{2}\z/
-    TIME_REGEXP = /\A(\d{2}):(\d{2}):(\d{2})\z/
-    DATE_TIME_REGEXP = /\A\d{4}-\d{2}-\d{2}T(\d{2}):(\d{2}):(\d{2})([\.,]\d+)?(Z|[+-](\d{2})(:?\d{2})?)?\z/
-
     DATE = "date"
     TIME = "time"
     DATE_TIME = "date-time"
 
-    FORMATS = {
-      DATE => DATE_REGEXP,
-      TIME => TIME_REGEXP,
-      DATE_TIME => DATE_TIME_REGEXP,
-    }.freeze
-
     CHARSET = Array('A'..'Z') + Array('a'..'z') + Array(0..9)
 
     def generate
-      return generate_string unless regex?
+      if format?
+        return generate_from_format
+      elsif pattern?
+        return generate_from_pattern
+      end
 
-      generate_from_regex
+      generate_string
     end
 
     def self.type
@@ -39,24 +34,51 @@ module Jfuzz
       Array.new(length) { CHARSET.sample }.join
     end
 
-    def generate_from_regex
-      if format.to_s.empty?
-        return Regexp.new(pattern).random_example
-      end
+    def generate_from_format
+      str = case format
+            when DATE
+              Time.at(rand * Time.now.to_i).to_date.to_s
+            when TIME
+              Time.at(rand * Time.now.to_i).to_datetime.to_s
+            when DATE_TIME
+              Time.at(rand * Time.now.to_i).to_s
+            else
+              raise "Unsupported format: #{format}"
+            end
 
-      FORMATS[format].random_example
+      if max_length?
+        return str[0..max_length - 1]
+      end
+      str
+    end
+
+    def generate_from_pattern
+      str = Regexp.new(pattern).random_example
+
+      if max_length?
+        return str[0..max_length - 1]
+      end
+      str
     end
 
     def max_length
-      property.fetch("maxLength", rand(min_length..100))
+      property.fetch("maxLength", rand(min_length..100)).to_i
     end
 
     def min_length
-      property.fetch("minLength", 1)
+      property.fetch("minLength", 1).to_i
     end
 
-    def regex?
-      !pattern.nil? || !format.nil?
+    def pattern?
+      !pattern.nil?
+    end
+
+    def format?
+      !format.nil?
+    end
+
+    def max_length?
+      !property.fetch("maxLength", nil).to_s.empty?
     end
 
     def pattern
